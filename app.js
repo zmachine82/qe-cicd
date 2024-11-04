1fd2dee3f4036605e14bc024dc846751cc05843a
Amis <ZACHARIAH.AMIS@VIZIENTINC.COM>
2024-11-04 14:22:12 -0600

    small refactor

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
 import { TaskList } from './task-list.js';
 
-class TaskRenderer {
-    constructor(taskList) {
-        this.taskList = taskList;
-    }
-
-    render() {
-        const ul = document.getElementById('todo-list');
-        ul.innerHTML = '';
-        this.taskList.tasks.forEach(task => {
-            const li = document.createElement('li');
-            li.textContent = task;
-            li.addEventListener('click', () => {
-                this.taskList.removeTask(task);
-                this.render();
-            });
-            ul.appendChild(li);
-        });
-    }
-}
-
 const taskList = new TaskList();
-const taskRenderer = new TaskRenderer(taskList);
 
-
+taskList.addObserver((tasks) => {
+    const ul = document.getElementById('todo-list');
+    ul.innerHTML = '';
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.textContent = task;
+        li.addEventListener('click', () => {
+            taskList.removeTask(task);
+        });
+        ul.appendChild(li);
+    });
+});
 
 document.getElementById('add-btn').addEventListener('click', () => {
     const task = document.getElementById('todo-input').value;
     taskList.addTask(task);
-    taskRenderer.render();
     document.getElementById('todo-input').value = '';
 });
--- task-list.js
+++ task-list.js
@@ -1,13 +1,36 @@
-export class TaskList {
+class Observable {
     constructor() {
+      this.observers = [];
+    }
+  
+    addObserver(observer) {
+      this.observers.push(observer);
+    }
+  
+    removeObserver(observer) {
+      this.observers = this.observers.filter(obs => obs !== observer);
+    }
+  
+    notify(data) {
+      this.observers.forEach(observer => observer(data));
+    }
+  }
+
+
+export class TaskList extends Observable {
+    constructor() {
+        super();
         this.tasks = [];
+        
     }
 
     addTask(task) {
         this.tasks.push(task);
+        this.notify(this.tasks);
     }
 
     removeTask(task) {
         this.tasks = this.tasks.filter(t => t !== task);
+        this.notify(this.tasks);
     }
 }
